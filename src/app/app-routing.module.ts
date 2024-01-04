982be2b8ba5aff19ddaa5f5dfd7e70a90b4afc05
Estefania <estefibbarria@gmail.com>
2024-01-04 16:41:45 -0300

    Navbar y opciones de este funcionales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'inicio',
+		pathMatch: 'full'
+	},
+	{ path: 'inicio', component: InicioComponent },
+	{ path: 'menu', component: MenuComponent },
+	{ path: 'nosotros', component: NosotrosComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    InicioComponent,
+    NosotrosComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
